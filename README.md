49fa3906060a78b70d1b94920a3c5a7d566f2a72
i321takeji <i.321.takeji@gmail.com>
2025-08-30 05:13:30 +0900

    Added up to Chapter 5

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # data-types-a-la-carte
+
+- Original
+  - Swierstra W, "Data types à la carte", Journal of Functional Programming, Vol 18, No 4, pp423--436 (2008)
+  - URL: <https://www.cambridge.org/core/journals/journal-of-functional-programming/article/data-types-a-la-carte/14416CB20C4637164EA9F77097909409>
+
+- お試し
+
+```sh
+$ stack repl
+```
---
+++ src/DataTypeALaCarte.hs
@@ -0,0 +1,164 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
+
+module DataTypeALaCarte where
+
+-- * 1 Introduction
+
+-- data Expr = Val Int | Add Expr Expr
+
+-- eval :: Expr -> Int
+-- eval (Val x) = x
+-- eval (Add x y) = eval x + eval y
+
+-- render :: Expr -> String
+-- render (Val x) = show x
+-- render (Add x y) = "(" ++ render x ++ " + " ++ render y ++ ")"
+
+-- * 2 Fixing the expression problem
+
+data Expr f = In (f (Expr f))
+
+data Val e = Val Int
+
+type IntExpr = Expr Val
+
+data Add e = Add e e
+
+type AddExpr = Expr Add
+
+data (f :+: g) e = Inl (f e) | Inr (g e)
+
+addExample :: Expr (Val :+: Add)
+addExample = In (Inr (Add (In (Inl (Val 118))) (In (Inl (Val 1219)))))
+
+-- * 3 Evaluation
+
+instance Functor Val where
+  fmap :: (a -> b) -> Val a -> Val b
+  fmap _ (Val x) = Val x
+
+instance Functor Add where
+  fmap :: (a -> b) -> Add a -> Add b
+  fmap f (Add e1 e2) = Add (f e1) (f e2)
+
+instance (Functor f, Functor g) => Functor (f :+: g) where
+  fmap :: (a -> b) -> (f :+: g) a -> (f :+: g) b
+  fmap f (Inl e1) = Inl (fmap f e1)
+  fmap f (Inr e2) = Inr (fmap f e2)
+
+foldExpr :: (Functor f) => (f a -> a) -> Expr f -> a
+foldExpr f (In t) = f (fmap (foldExpr f) t)
+
+class (Functor f) => Eval f where
+  evalAlgebra :: f Int -> Int
+
+instance Eval Val where
+  evalAlgebra :: Val Int -> Int
+  evalAlgebra (Val x) = x
+
+instance Eval Add where
+  evalAlgebra :: Add Int -> Int
+  evalAlgebra (Add x y) = x + y
+
+instance (Eval f, Eval g) => Eval (f :+: g) where
+  evalAlgebra :: (f :+: g) Int -> Int
+  evalAlgebra (Inl x) = evalAlgebra x
+  evalAlgebra (Inr y) = evalAlgebra y
+
+eval :: (Eval f) => Expr f -> Int
+eval expr = foldExpr evalAlgebra expr
+
+-- >>> eval addExample
+-- 1337
+
+-- * 4 Automating injections
+
+-- val :: Int -> Expr Val
+-- val x = In (Val x)
+
+infixl 6 |+|
+
+-- (|+|) :: Expr Add -> Expr Add -> Expr Add
+-- x |+| y = In (Add x y)
+
+-- val 1 |+| val 3 -- type error
+
+class (Functor sub, Functor sup) => sub :<: sup where
+  inj :: sub a -> sup a
+
+instance (Functor f) => f :<: f where
+  inj :: f a -> f a
+  inj = id
+
+instance {-# OVERLAPPING #-} (Functor f, Functor g) => f :<: (f :+: g) where
+  inj :: f a -> (f :+: g) a
+  inj = Inl
+
+instance {-# OVERLAPPABLE #-} (Functor f, Functor g, Functor h, f :<: g) => f :<: (h :+: g) where
+  inj :: f a -> (h :+: g) a
+  inj = Inr . inj
+
+inject :: (g :<: f) => g (Expr f) -> Expr f
+inject = In . inj
+
+val :: (Val :<: f) => Int -> Expr f
+val x = inject (Val x)
+
+(|+|) :: (Add :<: f) => Expr f -> Expr f -> Expr f
+x |+| y = inject (Add x y)
+
+ex1 :: Expr (Add :+: Val)
+ex1 = val 30000 |+| val 1330 |+| val 7
+
+-- >>> eval ex1
+-- 31337
+
+-- inVal :: Int -> Expr (Val :+: Val)
+-- inVal i = inject (Val i)
+
+-- ex0 :: Expr (Add :+: Val)
+-- ex0 = val 10 |+| val 20
+
+ex0' :: Expr (Add :+: Val)
+ex0' = In (Inl (Add (In (Inr (Val 10))) (In (Inr (Val 20)))))
+
+-- ex0comm :: Expr (Val :+: Add)
+-- ex0comm = permute commSum ex0
+
+-- * 5 Example
+
+data Mul x = Mul x x
+
+instance Functor Mul where
+  fmap :: (a -> b) -> Mul a -> Mul b
+  fmap f (Mul x y) = Mul (f x) (f y)
+
+instance Eval Mul where
+  evalAlgebra :: Mul Int -> Int
+  evalAlgebra (Mul x y) = x * y
+
+infixl 7 |*|
+
+(|*|) :: (Mul :<: f) => Expr f -> Expr f -> Expr f
+x |*| y = inject (Mul x y)
+
+-- >>> eval ex2
+-- 404
+-- ex2 :: Expr (Val :+: Add :+: Mul)
+-- ex2 = val 80 |*| val 5 |+| val 4
+
+infixr 6 :+:
+
+-- >>> eval ex3
+-- 42
+-- ex3 :: Expr (Val :+: Mul)
+-- ex3 = val 6 |*| val 7
+
+-- ex00 :: (Val :<: (Val :+: Add)) => Expr (Val :+: Add)
+-- ex00 :: (Val :<: (Add :+: Val)) => (Add :+: Val) f
+ex00 :: (Val :<: (g :+: Val)) => (g :+: Val) a
+ex00 = inj (Val 10)
